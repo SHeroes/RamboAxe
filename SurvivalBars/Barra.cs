54aa4c5ba826cb1446a3e615c4061319bd5596ce
SHeroes <danielgawron@cactus.ws>
2016-05-08 16:44:43 -0300

    falta corregir la brra de descarga. No falla pero no aparece

--- SurvivalBars/Barra.cs
+++ SurvivalBars/Barra.cs
@@ -1,20 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-/*
-using TgcViewer.Example;
-using TgcViewer;
-using Microsoft.DirectX.Direct3D;
-using System.Drawing;
-using Microsoft.DirectX;
-using TgcViewer.Utils.Modifiers;
-using TgcViewer.Utils._2D;
-using TgcViewer.Utils.TgcSceneLoader;
-using TgcViewer.Utils.TgcGeometry;
-using TgcViewer.Utils.Input;
-using Microsoft.DirectX.DirectInput;
-using System.Globalization;
-*/
 using Microsoft.DirectX;
 using TgcViewer;
 using Microsoft.DirectX.Direct3D;
@@ -31,72 +17,108 @@ namespace AlumnoEjemplos.SurvivalBars
         const float SELECTION_BOX_HEIGHT = 50;
         public static  int RED = 1;
         public static  int YELLOW = 2;
-       public static  int VIOLET = 3;
-       float finTiempoBarra;
-       float tiempoActualBarra;
-       
+        public static  int VIOLET = 3;
+        private static bool esBarraDeCarga;
+        private static bool ISACTIVE;
+        float finTiempoBarra;
+        float tiempoActualBarra;
+        float barraAnchoCompleto;
+        
 
-       public void init(int color , bool carga,float barraVaciaPosX = (float)60.0, float barraVaciaPosY = (float)460.0, float duracion = (float)3.00)
+        public bool esDeCarga() {
+            return Barra.esBarraDeCarga; 
+        }
+        public bool isActive()
         {
-            tiempoActualBarra = (float)0;
-            finTiempoBarra = duracion;
+            return Barra.ISACTIVE;
+        }
+
+
+        public void init(int color, bool carga, float barraVaciaPosX = (float)60.0, float barraVaciaPosY = (float)460.0, float duracion = (float)3.00)
+        {
+            Barra.esBarraDeCarga = carga;
             barEmpty = new TgcSprite();
             barColor = new TgcSprite();
             string colorPath = "";
-            switch (color){
-                case 1:     colorPath = "redBar.png";   break;
-                case 2:     colorPath = "yellowBar.png"; break;
-                case 3:     colorPath = "violet.png";   break;
-                default:    colorPath = "redBar.png";   break;
-            }
-
-
             float barraVaciaAlturaScaling = (float)0.26;
             float barraVaciaAnchoScaling = (float)0.26;
-           
- 
 
-
-            float bararAnchoCompleto = (float)0.23;
             float barraAlto = (float)0.28;
             float barraPosX = barraVaciaPosX + (float)19;
             float barraPosY = barraVaciaPosY + (float)3.5;
-
             float barrasWidth = 280;
+            barraAnchoCompleto = (float)0.23;
+            switch (color)
+            {
+                case 1: colorPath = "redBar.png"; break;
+                case 2: colorPath = "yellowBar.png"; break;
+                case 3: colorPath = "violet.png"; break;
+                default: colorPath = "redBar.png"; break;
+            }            
+            if (carga) {
+            
+                finTiempoBarra = duracion;
+                tiempoActualBarra = (float)0;
+            
+            }else {
 
+                finTiempoBarra = (float)0.000001;
+                tiempoActualBarra =  duracion;
+                barEmpty.Texture = TgcTexture.createTexture(GuiController.Instance.AlumnoEjemplosMediaDir + "\\survivalBars\\BarEmpty.png");
+                barEmpty.Position = new Vector2(barraVaciaPosX, barraVaciaPosY);
+                barEmpty.Scaling = new Vector2(barraVaciaAnchoScaling, barraVaciaAlturaScaling);
+                        
+            }
+            /*
             float vidaPorcentaje = (float)1;
             float vidaInicial = bararAnchoCompleto * vidaPorcentaje;
-
-            barEmpty.Texture = TgcTexture.createTexture(GuiController.Instance.AlumnoEjemplosMediaDir + "\\survivalBars\\BarEmpty.png");
+            */
             barColor.Texture = TgcTexture.createTexture(GuiController.Instance.AlumnoEjemplosMediaDir + "\\survivalBars\\" + colorPath);
-
-            barEmpty.Position = new Vector2(barraVaciaPosX, barraVaciaPosY);
-            barEmpty.Scaling = new Vector2(barraVaciaAnchoScaling, barraVaciaAlturaScaling);
             barColor.Position = new Vector2(barraPosX, barraPosY);
-            barColor.Scaling = new Vector2(bararAnchoCompleto, barraAlto);
+            barColor.Scaling = new Vector2(barraAnchoCompleto, barraAlto);
         
         }
         
         public void render(float elapsedTime){
-            float barraAnchoCompleto = (float)0.23;
+            float vidaActual;
 
-            float vidaActual = barraAnchoCompleto * ((finTiempoBarra - tiempoActualBarra) / finTiempoBarra);
-
-            tiempoActualBarra = tiempoActualBarra + elapsedTime;
-
-            if (tiempoActualBarra < finTiempoBarra)
+            if (!Barra.esBarraDeCarga)  //Barra DESCARGA
             {
-                //isActive ?
-                barColor.Scaling = new Vector2(vidaActual, (float)0.28);
-            }
-            barColor.render();
-            barEmpty.render();
 
+                vidaActual = barraAnchoCompleto * ((finTiempoBarra - tiempoActualBarra) / finTiempoBarra);
+                tiempoActualBarra = tiempoActualBarra + elapsedTime;
+
+                if (tiempoActualBarra < finTiempoBarra)
+                {
+                    //isActive ?
+                    barColor.Scaling = new Vector2(vidaActual, (float)0.28);
+                }
+                barEmpty.render();
+                barColor.render();
+
+            }
+            else {
+                vidaActual = barraAnchoCompleto * (tiempoActualBarra / finTiempoBarra);
+                tiempoActualBarra = tiempoActualBarra + elapsedTime;
+
+                if (tiempoActualBarra < finTiempoBarra)
+                {
+                    //isActive ?
+                    barColor.Scaling = new Vector2(vidaActual, (float)0.28);
+                    Barra.ISACTIVE = true;
+                }
+                else { 
+                //barra Cargada
+                    barColor.Scaling = new Vector2((float)0, (float)0.28);
+                    Barra.ISACTIVE = false;
+                }
+                barColor.render();            
+            }
         }
 
         public void dispose(){
-             barEmpty.dispose();
-             barColor.dispose();
+            if (!Barra.esBarraDeCarga)  barEmpty.dispose();
+            barColor.dispose();
         }
     }
 
--- SurvivalBars/BarraExampleTest.cs
+++ SurvivalBars/BarraExampleTest.cs
@@ -2,19 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using TgcViewer.Example;
+using System.Drawing;
+using Microsoft.DirectX.DirectInput;
 using TgcViewer;
+using TgcViewer.Example;
+using TgcViewer.Utils.Input;
+
 using Microsoft.DirectX.Direct3D;
 using System.Drawing;
 using Microsoft.DirectX;
+
 using TgcViewer.Utils.Modifiers;
+using TgcViewer.Utils._2D;
+using TgcViewer.Utils.TgcSceneLoader;
+using TgcViewer.Utils.TgcGeometry;
+using Microsoft.DirectX.DirectInput;
+using System.Globalization;
 
 namespace AlumnoEjemplos.SurvivalBars
 {
     public class BarraExampleTest : TgcExample
     {
         Barra barraEjemplo;
- 
+        Barra barraEjemplo2;
+        TgcText2d text3;
+
         public override string getCategory()
         {
             return "AlumnoEjemplos";
@@ -33,24 +45,37 @@ namespace AlumnoEjemplos.SurvivalBars
             //GuiController.Instance: acceso principal a todas las herramientas del Framework
 
             //Device de DirectX para crear primitivas
-            Device d3dDevice = GuiController.Instance.D3dDevice;
+            Microsoft.DirectX.Direct3D.Device d3dDevice = GuiController.Instance.D3dDevice;
             barraEjemplo = new Barra();
-            barraEjemplo.init(Barra.RED,false,160,360,10);
+            barraEjemplo.init(Barra.RED, false ,160,360,4);
+
+            barraEjemplo2 = new Barra();
+            barraEjemplo2.init(Barra.YELLOW, true, 100, 360, 10);
 
 
+            text3 = new TgcText2d();
+            text3.Text = "asdfasdf";
+            text3.Align = TgcText2d.TextAlign.LEFT;
+            text3.Position = new Point(5, 20);
+            text3.Size = new Size(310, 100);
+            text3.Color = Color.Gold;
         }
 
 
         public override void render(float elapsedTime)
         {
             //Device de DirectX para renderizar
-            Device d3dDevice = GuiController.Instance.D3dDevice;
+            Microsoft.DirectX.Direct3D.Device d3dDevice = GuiController.Instance.D3dDevice;
             GuiController.Instance.Drawer2D.beginDrawSprite();
 
             //Dibujar sprite (si hubiese mas, deberian ir todos aquí)
-
+            
+            text3.Text = barraEjemplo.isActive().ToString();
+            text3.render();
             barraEjemplo.render(elapsedTime);
 
+            //barraEjemplo2.render(elapsedTime);
+           //if (barraEjemplo.isActive()) barraEjemplo2.render(elapsedTime);
 
             //Finalizar el dibujado de Sprites
             GuiController.Instance.Drawer2D.endDrawSprite();
